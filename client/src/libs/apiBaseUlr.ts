1ff5474feac96202fe9512db6632641e04ef57be
unknown <arigbedeomobolaji@gmail.com>
2023-09-12 16:10:15 +0100

    finalizing things

--- client/src/libs/apiBaseUlr.ts
+++ client/src/libs/apiBaseUlr.ts
@@ -3,7 +3,7 @@ let apiBaseUrl;
 if (process.env.NODE_ENV === "production") {
 	apiBaseUrl = `https://sociopedia-ten.vercel.app`;
 } else {
-	apiBaseUrl = "";
+	apiBaseUrl = "http://127.0.0.1:8080";
 }
 
 export default apiBaseUrl;
