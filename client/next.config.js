f45d175e417a30bee04e6d53a5631775dc6e38f1
unknown <arigbedeomobolaji@gmail.com>
2023-09-12 19:27:26 +0100

    final is final

--- client/next.config.js
+++ client/next.config.js
@@ -22,7 +22,7 @@ const nextConfig = {
 					{ key: "Access-Control-Allow-Credentials", value: "true" },
 					{
 						key: "Access-Control-Allow-Origin",
-						value: "https://sociopedia-api-three.vercel.app/",
+						value: "https://sociopedia-api-three.vercel.app",
 					},
 					{
 						key: "Access-Control-Allow-Methods",
